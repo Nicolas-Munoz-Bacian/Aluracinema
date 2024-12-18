59974329f48fff631450f5cbd2b9e361fa11cb7d
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-18 13:40:59 -0300

    Add files via upload

--- src/routes.js
+++ src/routes.js
@@ -1,25 +1,23 @@
-import Inicio from "../src/pages/Inicio"
-import NotFound from "../src/pages/NotFound";
-import PaginaBase from "../src/pages/PaginaBase"
-import Player from "../src/pages/Player"
-import Favoritos from "../src/pages/Favoritos";
-import NuevaCard from "../src/pages/NuevaCard/NuevaCard";
+import Favoritos from "pages/Favoritos";
+import Inicio from "pages/Inicio";
+import NotFound from "pages/NotFound";
+import PaginaBase from "pages/PaginaBase";
+import Player from "pages/Player";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function AppRoutes(){
-    return(
-        <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<PaginaBase/>}>
-            <Route index element={<Inicio/>}></Route>
-            <Route path=":id" element={<Player/>}></Route>
-            <Route path="favoritos" element={<Favoritos />}></Route>
-            <Route path="NuevaCard" element={<NuevaCard />}></Route>
-            <Route path="*" element={<NotFound/>}></Route>
-            </Route>
-        </Routes>
-        </BrowserRouter>
-    )
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<PaginaBase />}>
+          <Route index element={<Inicio />}></Route>
+          <Route path="favoritos" element={<Favoritos />}></Route>
+          <Route path=":id" element={<Player />}></Route>
+          <Route path="*" element={<NotFound />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default AppRoutes;
