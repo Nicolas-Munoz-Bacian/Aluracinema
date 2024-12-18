191f0caa65893387e59025c74a5289aa4e9c5152
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-18 17:25:34 -0300

    Add files via upload

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,16 +1,15 @@
-import styles from "./Banner.module.css";
+import React from 'react';
+import styles from"../../components/Banner/Banner.module.css"
 
-function Banner({ img, color }) {
-  return (
-    <div
-      className={styles.capa}
-      style={{ backgroundImage: `url("/img/banner-${img}.png")` }}
-    >
-      <div className={styles.gradient} style={{ background: `${color}` }}>
-        
-      </div>
-    </div>
-  );
+function Banner ({img, color}){
+    return(
+        <div className={styles.capa} 
+        style={{backgroundImage: `url("/img/home.jpg")`}}
+        >
+        <div className={styles.gradient} style={{background:`${color}`}}>
+        </div>
+        </div>
+    );
 }
 
 export default Banner;
