a36ea283d8ab7005c3409dffdb7592a97a46a4fb
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-18 15:20:49 -0300

    Add files via upload

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -4,24 +4,22 @@ import Titulo from "components/Titulo";
 import Card from "components/Card";
 import { useFavoritosContext } from "context/Favoritos";
 
-  function Favoritos() {
-    const { favorito } = useFavoritosContext();
-  
-    return (
-      <>
+function Favoritos() {
+  const { favorito } = useFavoritosContext();
+
+  return (
+    <>
       <Banner img="favorite" color="#44d97d" />
       <Titulo>
         <h1>Mis favoritos</h1>
       </Titulo>
-        <section className={styles.container}>
-          {favorito.map((fav) => {
-            return <Card {...fav} key={fav.id} />;
-          })}
-        </section>
-      </>
-    );
-  }
-  
-  export default Favoritos;
-  
+      <section className={styles.container}>
+        {favorito.map((fav) => {
+          return <Card {...fav} key={fav.id} />;
+        })}
+      </section>
+    </>
+  );
+}
 
+export default Favoritos;
