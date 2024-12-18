901498a4cf9f81100e956916124685de84fe7448
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-18 17:32:22 -0300

    Add files via upload

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -3,52 +3,25 @@ import styles from "./Card.module.css";
 import iconFavorito from "./iconFavorito.png";
 import iconNoFavorito from "./iconNoFavorito.png";
 import { Link } from "react-router-dom";
-
-function Card({ id, capa, titulo, descripcion, video, onDelete, onSave, onClear }) {
-
-  const { favorito, agregarFavorito } = useFavoritosContext();
-  const [showModal, setShowModal] = useState(false);
-  const isFavorito = favorito.some(fav => fav.id === id);
-  const icon = isFavorito ? iconFavorito : iconNoFavorito;
-
-  const handleEdit = () => {
-      setShowModal(true);
-  };
+function Card({ id, capa, titulo }) {
   
-  const handleDelete = () => {
-      // Lógica para eliminar la tarjeta
-  };
+  const {favorito , agregarFavorito}= useFavoritosContext()
+  const isFavorito = favorito.some(fav=> fav.id === id)
+  const icon = isFavorito ? iconFavorito : iconNoFavorito 
 
   return (
-      <div className={styles.container}>
-          <Link to={`/player/${id}`}>
-              <img src={capa} alt={titulo} className={styles.capa} />
-              <h2>{titulo}</h2>
-          </Link>
-          <img 
-              src={icon} 
-              alt="Icono favorito"
-              className={styles.favorito}
-              onClick={() => agregarFavorito({ id, titulo, capa })}
-          />
-          <button onClick={handleEdit} className={styles.button}>
-              Editar
-          </button>
-          <button onClick={handleDelete} className={styles.button}>
-              Eliminar
-          </button>
-
-      {showModal && (
-          <Editmodal
-          initialData={{ id, titulo, capa, descripcion, video }}
-          onClose={() => setShowModal(false)}
-          onSave={onSave}
-          onDelete={onDelete}
-          onClear={onClear} // Pasa la función onClear al EditModal
+    <div className={styles.container}>
+      <Link className={styles.link} to={`/${id}`}>
+      <img src={capa} alt={titulo} className={styles.capa} />
+      <h2>{titulo}</h2>
+      </Link>
+      <img
+        src={icon}
+        alt="Icono favorito"
+        className={styles.favorito}
+        onClick={()=> agregarFavorito({id, titulo, capa})}
       />
-  )}
-</div>
-);
+    </div>
+  );
 }
-
 export default Card;
