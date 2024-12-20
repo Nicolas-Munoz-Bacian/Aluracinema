bffd615ebea59c9b50387087b6389b43b1ce39a7
Nicolas-Munoz-Bacian <nicolasmunoz2001@gmail.com>
2024-12-19 23:51:26 -0300

    Add files via upload

--- src/pages/Inicio/index.js
+++ src/pages/Inicio/index.js
@@ -1,11 +1,18 @@
-import Banner from "components/Banner";
-import Titulo from "components/Titulo";
-import Card from "components/Card";
-import styles from "./index.module.css";
+import Titulo from "../../components/Titulo";
+import Banner from "../../components/Banner";
+import Card from "../../components/Card";
+import styles from "../../pages/inicio/index.module.css";
+import home from "../../pages/inicio/home.jpg"
+import frontend from "../../pages/inicio/front end.png";
+import backend from "../../pages/inicio/back end.png";
+import innovacionYgestion from "../inicio/innovación y gestión.png";
 import { useState, useEffect } from "react";
 
 function Inicio() {
   const [videos, setVideos] = useState([]);
+  const [showModal, setShowModal] = useState(false); // Estado para controlar la visibilidad del modal
+  const [videoToEdit, setVideoToEdit] = useState(null); // Estado para guardar el video a editar
+  
 
   useEffect(() => {
     fetch(
@@ -16,18 +23,99 @@ function Inicio() {
         setVideos(data);
       });
   }, []);
+
+  const handleEdit = (video) => {
+    setVideoToEdit(video);
+    setShowModal(true);
+  };
+
+  const handleSave = (updatedVideo) => {
+    const updatedVideos = videos.map((video) => {
+      if (video.id === updatedVideo.id) {
+        return updatedVideo;
+      }
+      return video;
+    });
+    setVideos(updatedVideos);
+    setShowModal(false);
+    setVideoToEdit(null);
+  };
+
+  const addVideo = (newVideo) => {
+    setVideos([...videos, newVideo]);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+    setVideoToEdit(null);
+  };
+
+  const handleDelete = (videoId) => {
+    const updatedVideos = videos.filter((video) => video.id !== videoId);
+    setVideos(updatedVideos);
+  };
+
+  const handleClear = () => {
+    setVideoToEdit(null);
+    setShowModal(false);
+  };
+  
+
   return (
     <>
-      <Banner img="home" color="#154580" />
+      <Banner src={home} img="home" color="#154580" />
+
+      {/* Sección Front End */}
       <Titulo>
-        <h1>Un lugar para guardar sus videos favoritos </h1>
+        <img src={frontend} className="banner" alt="banner front end" />
       </Titulo>
 
       <section className={styles.container}>
-        {videos.map((video) => {
-          return <Card {...video} key={video.id} />;
-        })}
+        {videos.map((video) => (
+        <Card
+        {...video}
+        key={video.id}
+        onEdit={() => handleEdit(video)}
+        onDelete={() => handleDelete(video.id)}
+        onSave={handleSave}
+        onClear={handleClear} // Pasa la función handleClear como prop
+      />
+    ))}
       </section>
+
+      {/* Sección Back End */}
+      <Titulo>
+        <img src={backend} className="banner" alt="banner back end" />
+      </Titulo>
+
+      <section className={styles.container}>
+        {videos.map((video) => (
+        <Card
+        {...video}
+        key={video.id}
+        onEdit={() => handleEdit(video)}
+        onDelete={() => handleDelete(video.id)} // Pasa la función handleDelete como prop
+      />
+        ))}
+      </section>
+
+      {/* Sección Innovación y Gestión */}
+      <Titulo>
+        <img src={innovacionYgestion} className="banner" alt="banner innovación y gestion" />
+      </Titulo>
+      <section className={styles.container}>
+        {videos.map((video) => (
+        <Card
+        {...video}
+        key={video.id}
+        onEdit={() => handleEdit(video)}
+        onDelete={() => handleDelete(video.id)} // Pasa la función handleDelete como prop
+      />
+        ))}
+      </section>
+      <>
+    </>
+
     </>
   );
 }
